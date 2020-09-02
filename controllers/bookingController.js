6511d7a9928171f6388ba39bca5246ebafc4ef40
Ayush Kumar <kumarayush731@gmail.com>
2020-09-02 12:47:11 +0530

    fix bug of stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,6 +20,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    amount: tour.price * 100,
+    currency: 'usd',
     line_items: [
       {
         name: `${tour.name} Tour`,
@@ -27,8 +29,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         images: [
           `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
         ],
-        amount: tour.price * 100,
-        currency: 'usd',
+
         quantity: 1,
       },
     ],
